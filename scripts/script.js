8ca0a749cbfd7c8d23549f6d0b81ec2027ea659f
Wesley Ting <wesleyting3@gmail.com>
2020-11-02 16:39:47 -0800

    Added action to home button

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,13 @@
-var toggleBtn = document.querySelector(".sidebar-toggle");
-var sidebar = document.querySelector(".sidebar");
+var toggleBtn = document.querySelector('.sidebar-toggle');
+var sidebar = document.querySelector('.sidebar');
 
-toggleBtn.addEventListener("click", function () {
-  toggleBtn.classList.toggle("is-closed");
-  sidebar.classList.toggle("is-closed");
+toggleBtn.addEventListener('click', function() {
+  toggleBtn.classList.toggle('is-closed');
+  sidebar.classList.toggle('is-closed');
+})
+
+
+//adding the action to the button
+$(document).on('click', '#moveToHome', function(){
+  fullpage_api.moveTo('Home');
 });
