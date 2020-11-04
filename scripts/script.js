76f3220850f079f7c21f512d825e977220a0cc03
Wesley Ting <wesleyting3@gmail.com>
2020-11-03 16:23:05 -0800

    Adding fullpage links

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,33 @@
-var toggleBtn = document.querySelector('.sidebar-toggle');
-var sidebar = document.querySelector('.sidebar');
+var toggleBtn = document.querySelector('.s1');
+var sidebar = document.querySelector('.t1');
 
 toggleBtn.addEventListener('click', function() {
   toggleBtn.classList.toggle('is-closed');
   sidebar.classList.toggle('is-closed');
 })
 
+var toggleBtn2 = document.querySelector('.s2');
+var sidebar2 = document.querySelector('.t2');
 
-//adding the action to the button
+toggleBtn2.addEventListener('click', function() {
+  toggleBtn2.classList.toggle('is-closed');
+  sidebar2.classList.toggle('is-closed');
+})
+
+
 $(document).on('click', '#moveToHome', function(){
   fullpage_api.moveTo('Home');
 });
+
+$(document).on('click', '#moveToAbout', function(){
+  fullpage_api.moveTo('About');
+});
+
+$(document).on('click', '#moveToWorks', function(){
+  fullpage_api.moveTo('Works');
+});
+
+$(document).on('click', '#moveToContact', function(){
+  fullpage_api.moveTo('Contact');
+});
+
